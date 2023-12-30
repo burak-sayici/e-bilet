342ba99407e7523801f98e577cad58fc32a053b1
Burak SAYICI <155234322+burak-sayici@users.noreply.github.com>
2023-12-30 21:08:49 +0000

    pipline

--- EBilet/Controllers/WeatherForecastController.cs
+++ EBilet/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace EBilet.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing test br", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing test pipline", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
